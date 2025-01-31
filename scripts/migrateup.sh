16e2c556a7f3aa0788a3d3682f63ac2bd1a02f0e
jamin-samuel <jamin.fernandez@finoservices.com>
2025-01-31 14:25:33 -0500

    yaaaaaaaaaaaaA

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Cargar variables de entorno desde .env si el archivo existe
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-
 # Cambiar al directorio de esquemas SQL
 cd sql/schema
 
-# Ejecutar goose con la URL entrecomillada
-goose -v turso "$DATABASE_URL" up
+# Ejecutar goose con la URL directamente
+goose -v turso "libsql://notely-db-cc-jamin-fernandez.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzgzNDkyNTUsImlkIjoiMmViN2I2MDgtMzAxYi00Y2FkLWI0MjYtZWIzNGNjZDMyNTg3In0.dar0b2mWcxzhHXDiZ-37uvPkQ7QtSW7W_XMA9jS-_VWnb0OfkskPO5GCUErfRdTUQ-KaDLBYIgS2ANeZVpl2AQ" up
