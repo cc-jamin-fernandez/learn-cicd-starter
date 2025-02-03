365aea57583aedda4f3621338e4799e66cb80403
jamin-samuel <jamin.fernandez@finoservices.com>
2025-02-03 10:13:15 -0500

    cambios de hoy

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Cambiar al directorio de esquemas SQL
-cd sql/schema
+if [ -f .env ]; then
+    source .env
+fi
 
-# Ejecutar goose con la URL directamente
-goose -v turso "libsql://notely-db-cc-jamin-fernandez.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzgzNDkyNTUsImlkIjoiMmViN2I2MDgtMzAxYi00Y2FkLWI0MjYtZWIzNGNjZDMyNTg3In0.dar0b2mWcxzhHXDiZ-37uvPkQ7QtSW7W_XMA9jS-_VWnb0OfkskPO5GCUErfRdTUQ-KaDLBYIgS2ANeZVpl2AQ" up
+cd sql/schema
+goose turso $DATABASE_URL up
